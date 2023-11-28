55f3c4401828433336262ed957895c56c0eb55da
Nathan Lenogue <82942062+NatLno@users.noreply.github.com>
2023-11-28 17:07:02 +0100

    feat: add vr camera

---
+++ sdsim/Assets/CheckLap.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckLap : MonoBehaviour
+{
+    [SerializeField]
+    int m_nbLap = 0;
+
+    [SerializeField]
+    bool m_beginLap;
+
+    [SerializeField]
+    GameObject m_portalCheckpoint;
+    
+    [SerializeField]
+    CheckLap m_nextCheckPoint;
+
+    [SerializeField]
+    List<CheckLap> m_checkPoints;
+
+    [SerializeField]
+    TimePanel m_timePanel;
+
+    bool m_trigger = false;
+
+    private void Start()
+    {
+        foreach (CheckLap checkPoint in m_checkPoints)
+        {
+            checkPoint.m_portalCheckpoint.SetActive(false);
+        }
+        if (m_checkPoints.Count > 0)
+            m_checkPoints[0].m_portalCheckpoint.SetActive(true);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Car"))
+        {
+            if (!m_beginLap)
+            {
+                if (m_portalCheckpoint.activeInHierarchy && !m_trigger) { 
+                    m_trigger = true;
+                    m_portalCheckpoint.SetActive(false);
+                    m_nextCheckPoint.m_portalCheckpoint.SetActive(true);
+                }
+            }
+
+            if (m_beginLap && CheckAllCheckpoint())
+            {
+                m_nbLap++;
+                if (m_timePanel != null)
+                {
+                    m_timePanel.SetNbLap(m_nbLap);
+                    SetLapTime();
+                }
+                ResetCheckPoints();
+            }
+        }
+    }
+
+    private bool CheckAllCheckpoint()
+    {
+        foreach (CheckLap checkPoint in m_checkPoints)
+        {
+            if (!checkPoint.m_trigger)
+                return false;
+        }
+        return true;
+    }
+
+    private void ResetCheckPoints()
+    {
+        foreach (CheckLap checkPoint in m_checkPoints)
+        {
+            checkPoint.m_trigger = false;
+        }
+    }
+
+    void SetLapTime()
+    {
+        Timer timer = GameObject.FindObjectOfType<Timer>();
+        if (timer)
+        {
+            m_timePanel.SetBestLapTime();
+            m_timePanel.SetNextLap(m_nbLap.ToString());
+            timer.DisableTimer();
+            timer.StartTimer();
+        }
+    }
+}
---
+++ sdsim/Assets/TimePanel.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimePanel : MonoBehaviour
+{
+    [SerializeField]
+    Text m_nbLap;
+
+    [SerializeField]
+    Text m_timeLap;
+    
+    [SerializeField]
+    Text m_bestTimeLap;
+
+    [SerializeField]
+    GameObject m_time;
+
+    [SerializeField]
+    GameObject m_parentTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetNbLap(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer timer = GameObject.FindObjectOfType<Timer>();
+        if (timer != null)
+            m_timeLap.text = timer.currentTotTimeDisp.text;
+    }
+
+    public void SetNbLap(int value)
+    {
+        m_nbLap.text = value.ToString();
+    }
+
+    public void SetNextLap(string nbLap)
+    {
+        GameObject timeObject = Object.Instantiate(m_time, m_parentTime.transform);
+        m_timeLap = timeObject.GetComponentsInChildren<Text>()[1];
+        timeObject.GetComponentsInChildren<Text>()[0].text = "Lap n°" + nbLap + ":";
+    }
+
+    public int TimeStrToSeconde(string strTime)
+    {
+        string[] tabStr = strTime.Split('.');
+        int minutes = int.Parse(tabStr[0]);
+        int secondes = int.Parse(tabStr[1]);
+        return minutes * 60 + secondes;
+    }
+
+    public void SetBestLapTime()
+    {
+        if (m_bestTimeLap.text.Equals("--:--"))
+        {
+            m_bestTimeLap.text = m_timeLap.text;
+            return;
+        }
+        int lastTime = TimeStrToSeconde(m_bestTimeLap.text);
+        int newTime = TimeStrToSeconde(m_timeLap.text);
+        if (newTime < lastTime)
+            m_bestTimeLap.text = m_timeLap.text;
+    }
+}
